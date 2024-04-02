0ca12a74d9b3bae1b61fd1477ae6059ee5ac2006
Notsickfox <abhinav.variyath15941@gmail.com>
2024-04-02 20:46:49 +0530

    add requests

--- src/api.ts
+++ src/api.ts
@@ -4,6 +4,8 @@ import { default as fetch } from "node-fetch"
 import { ShazamRoot } from "./types/shazam.js";
 import { s16LEToSamplesArray } from "./utils.js";
 import fs from 'fs'
+import { Device,Request,ShazamURLS } from "./requests.js";
+import { USER_AGENTS } from "./useragents.js";
 import { convertfile } from "./to_pcm.js";
 const TIME_ZONE = "Europe/Paris";
 
@@ -36,25 +38,17 @@ export class Endpoint{
             'video': 'v3'
         };
     }
-    headers(){
-        return {
-            "X-Shazam-Platform": "IPHONE",
-            "X-Shazam-AppVersion": "14.1.0",
-            "Accept": "*/*",
-            "Content-Type": "application/json",
-            "Accept-Encoding": "gzip, deflate",
-            "Accept-Language": "en",
-            "User-Agent": "Shazam/3685 CFNetwork/1197 Darwin/20.0.0"
-        }
+    headers(language: string = 'en'){
+        return Request.headers(language)
     }
 
 
-    async sendRecognizeRequest(url: string, body: string): Promise<ShazamRoot | null> {
+    async sendRecognizeRequest(url: string, body: string, language: string = 'en'): Promise<ShazamRoot | null> {
         //@ts-ignore
-        return await (await fetch(url, { body, headers: this.headers(), method: "POST" })).json();
+        return await (await fetch(url, { body, headers: this.headers(language), method: "POST" })).json();
     }
 
-    async formatAndSendRecognizeRequest(signature: DecodedMessage): Promise<ShazamRoot | null>{
+    async formatAndSendRecognizeRequest(signature: DecodedMessage, language: string = 'en'): Promise<ShazamRoot | null>{
         let data = {
             'timezone': this.timezone,
             'signature': {
@@ -68,7 +62,7 @@ export class Endpoint{
         const url = new URL(this.url());
         Object.entries(this.params()).forEach(([a, b]) => url.searchParams.append(a, b));
 
-        let response = await this.sendRecognizeRequest(url.toString(), JSON.stringify(data));
+        let response = await this.sendRecognizeRequest(url.toString(), JSON.stringify(data), language);
         if(response?.matches.length === 0) return null;
 
         return response as ShazamRoot;
@@ -83,20 +77,31 @@ export class Shazam{
         this.endpoint = new Endpoint(timeZone ?? TIME_ZONE);
     }
 
-    
-    async fromFilePath(path: string){
+    headers(language: string = 'en'){
+        return Request.headers(language)
+    }
+
+    async fromFilePath(path: string, minimal: Boolean = false, language: string = 'en'){
         await convertfile(path)
         const data = fs.readFileSync('output.pcm')
         
         const conv = s16LEToSamplesArray(data)
         fs.unlinkSync('output.pcm')
-        const recognise = await this.recognizeSong(conv)
+        const recognise = minimal ? await this.recognizeSongMinimal(conv,language) : await this.recognizeSong(conv,language)
         return recognise
 
     }
 
-    async recognizeSong(samples: number[], callback?: ((state: "generating" | "transmitting") => void)){
-        let response = await this.fullRecognizeSong(samples, callback);
+    async recognizeSong(samples: number[], language: string = 'en', callback?: ((state: "generating" | "transmitting") => void)){
+        let response = await this.fullRecognizeSong(samples, callback, language);
+        if(!response) return null;
+
+        return response
+
+    }
+
+    async recognizeSongMinimal(samples: number[], language: string = 'en', callback?: ((state: "generating" | "transmitting") => void)){
+        let response = await this.fullRecognizeSong(samples, callback, language);
         if(!response) return null;
 
         const
@@ -111,7 +116,7 @@ export class Shazam{
 
     }
 
-    async fullRecognizeSong(samples: number[], callback?: ((state: "generating" | "transmitting") => void)){
+    async fullRecognizeSong(samples: number[], callback?: ((state: "generating" | "transmitting") => void), language: string = 'en'){
         callback?.("generating");
         let generator = this.createSignatureGenerator(samples);
         while(true){
@@ -121,7 +126,7 @@ export class Shazam{
                 break;
             }
             callback?.("transmitting");
-            let results = await this.endpoint.formatAndSendRecognizeRequest(signature);
+            let results = await this.endpoint.formatAndSendRecognizeRequest(signature, language);
             if(results !== null) return results;
         }
         return null;
@@ -132,4 +137,76 @@ export class Shazam{
         signatureGenerator.feedInput(samples);
         return signatureGenerator;
     }
+
+    async top_tracks_global(language: string = 'en-US',endpoint_country: string = 'GB',limit: string = '10',offset: string = '0'){
+
+        const url = ShazamURLS.top_tracks_global(language,endpoint_country,limit,offset)
+        return await (await fetch(url, { headers: this.headers(language), method: "GET" })).json();
+    }
+
+    async top_tracks_country(language: string,endpoint_country: string,country_code: string,limit: string,offset: string){
+
+        const url = ShazamURLS.top_tracks_country(language,endpoint_country,country_code,limit,offset)
+        return await (await fetch(url, { headers: this.headers(language), method: "GET" })).json();
+    }
+
+    async top_tracks_city(language: string,endpoint_country: string,city_id: string,limit: string,offset: string){
+
+        const url = ShazamURLS.top_tracks_city(language,endpoint_country,city_id,limit,offset)
+        return await (await fetch(url, { headers: this.headers(language), method: "GET" })).json();
+    }
+
+    async track_info(language: string,endpoint_country: string,track_id: string){
+
+        const url = ShazamURLS.track_info(language,endpoint_country,track_id)
+        return await (await fetch(url, { headers: this.headers(language), method: "GET" })).json();
+    }
+
+    async list_locations(){
+        const url = ShazamURLS.locations()
+        return await (await fetch(url, { headers: this.headers(), method: "GET" })).json();
+    }
+
+    async top_genre_tracks_world(language: string,endpoint_country: string,genre: string,limit: string,offset: string){
+
+        const url = ShazamURLS.genre_world(language,endpoint_country,genre,limit,offset)
+        return await (await fetch(url, { headers: this.headers(language), method: "GET" })).json();
+    }
+
+    async top_genre_tracks_country(language: string, endpoint_country: string, country: string, genre: string, limit: string, offset: string){
+
+        const url = ShazamURLS.genre_country(language,endpoint_country,country,genre,limit,offset)
+        return await (await fetch(url, { headers: this.headers(language), method: "GET" })).json();
+    }
+
+    async related_songs(language: string,endpoint_country: string,track_id: string,offset: string,limit: string){
+
+        const url = ShazamURLS.related_songs(language,endpoint_country,track_id,offset,limit)
+        return await (await fetch(url, { headers: this.headers(language), method: "GET" })).json();
+    }
+
+    async search_artist(language: string,endpoint_country: string,query: string,limit: string,offset: string){
+
+        const url = ShazamURLS.search_artist(language,endpoint_country,query,limit,offset)
+        return await (await fetch(url, { headers: this.headers(language), method: "GET" })).json();
+    }
+
+    async search_artist_v2(endpoint_country: string,artist_id: string){
+
+        const url = ShazamURLS.search_artist_v2(endpoint_country,artist_id)
+        return await (await fetch(url, { headers: this.headers(), method: "GET" })).json();
+    }
+
+    async artist_albums(endpoint_country: string,artist_id: string,limit: string,offset: string){
+
+        const url = ShazamURLS.artist_albums(endpoint_country,artist_id,limit,offset)
+        return await (await fetch(url, { headers: this.headers(), method: "GET" })).json();
+    }
+
+    async search_music(language: string,endpoint_country: string, query: string,limit: string,offset: string){
+
+        const url = ShazamURLS.search_music(language,endpoint_country,query,limit,offset)
+        return await (await fetch(url, { headers: this.headers(language), method: "GET" })).json();
+    }
+
 }
--- src/requests.ts
+++ src/requests.ts
@@ -1,78 +1,79 @@
-import { USER_AGENTS } from "./useragents"
+import { USER_AGENTS } from "./useragents.js"
 
 export class ShazamURLS{
 
-    search_from_file(language: string,endpoint_country: string,device: string,uuid_1: string,uuid_2: string){
+    /*search_from_file(language: string,endpoint_country: string,device: string,uuid_1: string,uuid_2: string){
         return `https://amp.shazam.com/discovery/v5/${language}/${endpoint_country}/${device}/-/tag/${uuid_1}/${uuid_2}?sync=true&webv3=true&sampling=true&connected=&shazamapiversion=v3&sharehub=true&hubv5minorversion=v5.1&hidelb=true&video=v3`
-    }
+    }*/
 
-    top_tracks_global(language: string,endpoint_country: string,limit: string,offset: string){
+    static top_tracks_global(language: string,endpoint_country: string,limit: string,offset: string){
         return `https://www.shazam.com/shazam/v3/${language}/${endpoint_country}/web/-/tracks/ip-global-chart?pageSize=${limit}&startFrom=${offset}`
     }
 
-    track_info(language: string,endpoint_country: string,track_id: string){
+    static track_info(language: string,endpoint_country: string,track_id: string){
         return `https://www.shazam.com/discovery/v5/${language}/${endpoint_country}/web/-/track/${track_id}?shazamapiversion=v3&video=v3 `
     }
 
-    top_tracks_country(language: string,endpoint_country: string,country_code: string,limit: string,offset: string){
+    static top_tracks_country(language: string,endpoint_country: string,country_code: string,limit: string,offset: string){
         return `https://www.shazam.com/shazam/v3/${language}/${endpoint_country}/web/-/tracks/ip-country-chart-${country_code}?pageSize=${limit}&startFrom=${offset}`
     }
 
-    top_tracks_city(language: string,endpoint_country: string,city_id: string,limit: string,offset: string){
+    static top_tracks_city(language: string,endpoint_country: string,city_id: string,limit: string,offset: string){
         return `https://www.shazam.com/shazam/v3/${language}/${endpoint_country}/web/-/tracks/ip-city-chart-${city_id}?pageSize=${limit}&startFrom=${offset}`
     }
 
-    locations(){
+    static locations(){
         return `https://www.shazam.com/services/charts/locations`
     }
 
-    genre_world(language: string,endpoint_country: string,genre: string,limit: string,offset: string){
+    static genre_world(language: string,endpoint_country: string,genre: string,limit: string,offset: string){
         return `https://www.shazam.com/shazam/v3/${language}/${endpoint_country}/web/-/tracks/genre-global-chart-${genre}?pageSize=${limit}&startFrom=${offset}`
     }
 
-    genre_country(language: string, endpoint_country: string, country: string, genre: string, limit: string, offset: string){
+    static genre_country(language: string, endpoint_country: string, country: string, genre: string, limit: string, offset: string){
         return `https://www.shazam.com/shazam/v3/${language}/${endpoint_country}/web/-/tracks/genre-country-chart-${country}-${genre}?pageSize=${limit}&startFrom=${offset}`
     }
 
-    related_songs(language: string,endpoint_country: string,track_id: string,offset: string,limit: string){
+    static related_songs(language: string,endpoint_country: string,track_id: string,offset: string,limit: string){
         return `https://cdn.shazam.com/shazam/v3/${language}/${endpoint_country}/web/-/tracks/track-similarities-id-${track_id}?startFrom=${offset}&pageSize=${limit}&connected=&channel=`
     }
 
-    search_artist(language: string,endpoint_country: string,query: string,limit: string,offset: string){
+    static search_artist(language: string,endpoint_country: string,query: string,limit: string,offset: string){
         return `https://www.shazam.com/services/search/v4/${language}/${endpoint_country}/web/search?term=${query}&limit=${limit}&offset=${offset}&types=artists`
     }
 
-    search_music(language: string,endpoint_country: string, query: string,limit: string,offset: string){
+    static search_music(language: string,endpoint_country: string, query: string,limit: string,offset: string){
         return `https://www.shazam.com/services/search/v3/${language}/${endpoint_country}/web/search?query=${query}&numResults=${limit}&offset=${offset}&types=songs`
     }
 
-    listening_counter(){
+    static listening_counter(){
         return `https://www.shazam.com/services/count/v2/web/track/{}`
     }
 
-    listening_counter_many(){
+    static listening_counter_many(){
         return `https://www.shazam.com/services/count/v2/web/track`
     }
 
-    search_artist_v2(endpoint_country: string,artist_id: string){
+    static search_artist_v2(endpoint_country: string,artist_id: string){
         return `https://www.shazam.com/services/amapi/v1/catalog/${endpoint_country}/artists/${artist_id}`
     }
 
-    artist_albums(endpoint_country: string,artist_id: string,limit: string,offset: string){
+    static artist_albums(endpoint_country: string,artist_id: string,limit: string,offset: string){
         return `https://www.shazam.com/services/amapi/v1/catalog/${endpoint_country}/artists/${artist_id}/albums?limit=${limit}&offset=${offset}`
     }
     
 }
 
 export class Request{
-    headers(language: string){
+    static headers(language: string = 'en'){
         return {
             "X-Shazam-Platform": "IPHONE",
             "X-Shazam-AppVersion": "14.1.0",
             "Accept": "*/*",
-            "Accept-Language": language,
+            "Content-Type": "application/json",
             "Accept-Encoding": "gzip, deflate",
-            "User-Agent": USER_AGENTS[Math.floor(Math.random() * USER_AGENTS.length)]
+            "Accept-Language": language,
+            "User-Agent": `${USER_AGENTS[Math.floor(Math.random() * USER_AGENTS.length)]}`
         }
     }
 }
